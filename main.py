c6c2fa88299e67d11a92dc241d858d0cbb6c5f5d
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2026-05-29 19:50:21 -0400

    Integrated with deepagent ui

--- main.py
+++ main.py
@@ -10,15 +10,7 @@ load_dotenv()
 langfuse = get_client()
 langfuse.flush()
 
-@observe()
-def main():
-    agent = create_deep_agent(
-        model=f"openai:{os.getenv('OPENAI_MODEL')}",
-        system_prompt="You are a research assistant.",
-    )
-    result = agent.invoke({"messages": "Research LangGraph and write a summary"})
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
+agent = create_deep_agent(
+    model=f"openai:{os.getenv('OPENAI_MODEL')}",
+    system_prompt="You are a research assistant.",
+)
